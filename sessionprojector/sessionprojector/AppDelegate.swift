abbe63c3bd40f19b3678ebcf46ed363d2b0c2260
Gyuhwan Park <unstabler@unstabler.pl>
2022-05-19 18:02:40 +0900

    feature(sessionprojector): add tray icon / menu

--- sessionprojector/sessionprojector/AppDelegate.swift
+++ sessionprojector/sessionprojector/AppDelegate.swift
@@ -16,21 +16,26 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     let eventInjector = EventInjector()
     let projectionServer = ProjectionServer()
 
+    lazy var trayStatusIndicator = NSMenuItem(title: "", action: nil, keyEquivalent: "")
+    lazy var trayMenu: NSMenu = {
+        let menu = NSMenu()
+
+        menu.addItem(trayStatusIndicator)
+        menu.addItem(withTitle: "Quit", action: #selector(quitApplication), keyEquivalent: "q")
+
+        return menu
+    }()
+
+    let trayItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.squareLength)
+
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         // Insert code here to initialize your application
         projectionServer.delegate = self
 
-        Task {
-            do {
-                try await screenRecorder.prepare()
-                try await screenRecorder.start()
-            } catch {
-                print(error.localizedDescription)
-            }
-            
-            await window.close()
-            projectionServer.start()
-        }
+        initializeTrayItem()
+        updateTrayStatusIndicator()
+
+        startProjectionServer()
     }
 
 
@@ -42,16 +47,50 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
         return true
     }
+
+    func startProjectionServer() {
+        Task {
+            do {
+                try await screenRecorder.prepare()
+                try await screenRecorder.start()
+            } catch {
+                print(error.localizedDescription)
+            }
+
+            projectionServer.start()
+        }
+    }
+
+    func destroyProjectionServer() {
+
+    }
+
+    @objc
+    func quitApplication() {
+        NSApp.terminate(self)
+    }
+
+    func initializeTrayItem() {
+        trayItem.menu = trayMenu
+    }
+
+    func updateTrayStatusIndicator() {
+        trayStatusIndicator.title = "\(projectionServer.sessions.count) connection(s)"
+    }
 }
 
 extension AppDelegate: ProjectionServerDelegate {
     func projectionServer(sessionInitiated session: ProjectionSession, id: UInt64) {
         screenRecorder.subscribeUpdate(session)
         session.eventInjector = eventInjector
+
+        updateTrayStatusIndicator()
     }
 
     func projectionServer(sessionClosed session: ProjectionSession, id: UInt64) {
         screenRecorder.unsubscribeUpdate(session)
         session.eventInjector = nil
+
+        updateTrayStatusIndicator()
     }
 }
