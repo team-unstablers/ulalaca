d66d9fd20cf769391c26151c6f23b97b59b4f0ae
Gyuhwan Park <unstabler@unstabler.pl>
2022-07-31 19:56:20 +0900

    feature(ulalacacore/ipc): add message for output supression

--- ulalacacore/UlalacaCore/ipc/messages/projector.h
+++ ulalacacore/UlalacaCore/ipc/messages/projector.h
@@ -17,6 +17,12 @@ static const uint16_t TYPE_EVENT_MOUSE_MOVE   = 0x0321;
 static const uint16_t TYPE_EVENT_MOUSE_BUTTON = 0x0322;
 static const uint16_t TYPE_EVENT_MOUSE_WHEEL  = 0x0323;
 
+static const uint16_t TYPE_PROJECTION_START        = 0x0401;
+static const uint16_t TYPE_PROJECTION_STOP         = 0x0402;
+
+static const uint16_t TYPE_PROJECTION_SET_VIEWPORT = 0x0421;
+
+
 /* constants: Screen update notification */
 static const uint8_t SCREEN_UPDATE_NOTIFY_TYPE_ENTIRE_SCREEN = 0;
 static const uint8_t SCREEN_UPDATE_NOTIFY_TYPE_PARTIAL = 1;
@@ -93,6 +99,20 @@ struct ULIPCMouseWheelEvent {
     uint16_t flags;
 } MARK_AS_PACKED_STRUCT;
 
+struct ULIPCProjectionStart {
+    uint16_t flags;
+} MARK_AS_PACKED_STRUCT;
 
+struct ULIPCProjectionStop {
+    uint16_t flags;
+} MARK_AS_PACKED_STRUCT;
+
+struct ULIPCProjectionSetViewport {
+    uint8_t monitorId;
+    uint16_t width;
+    uint16_t height;
+
+    uint16_t flags;
+} MARK_AS_PACKED_STRUCT;
 
 #endif
